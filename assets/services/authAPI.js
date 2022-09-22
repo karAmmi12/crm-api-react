9a6377c087fbcc6f935e77dce52f7e39706641cb
karim <karim.ammi@hotmail.com>
2022-09-22 14:27:27 +0200

    centralisation et configuration des adresses API

--- assets/services/authAPI.js
+++ assets/services/authAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import { LOGIN_API } from "../config";
 
 
 /**
@@ -15,7 +16,7 @@ function logout(){
 
 function authenticate(credentials){
     return axios
-         .post("http://localhost:8000/api/login_check", credentials)
+         .post(LOGIN_API, credentials)
          .then(response => response.data.token)
          .then(token => {
             //je stock le token dans mon localStorage       
